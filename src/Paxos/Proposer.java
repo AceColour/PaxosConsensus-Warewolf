2a57105e9400993cb6da31f827dfd7f1e16a54d8
Erick Chandra <erickchandra_1@yahoo.com>
2016-04-29 20:45:42 +0700

    Implemented Proposer draft.

--- src/Paxos/Proposer.java
+++ src/Paxos/Proposer.java
@@ -1,5 +1,7 @@
 package Paxos;
 
+import java.util.HashSet;
+
 /**
  * Created by erickchandra on 4/25/16.
  *
@@ -7,13 +9,16 @@ package Paxos;
  */
 public class Proposer {
     // Attributes
+    protected Messenger messenger;
     protected ProposalId proposalId;
-    protected Object proposedValue = null;
+    protected int proposedValue = 0;
     protected ProposalId lastAcceptedId = null;
     protected final int quorumSize;
+    protected HashSet<Integer> promisesReceived = new HashSet<Integer>();
 
     // Constructor
-    public Proposer(int _quorumSize) {
+    public Proposer(Messenger _messenger, int _quorumSize) {
+        this.messenger = _messenger;
         this.quorumSize = _quorumSize;
         this.proposalId = new ProposalId(0);
     }
@@ -50,11 +55,34 @@ public class Proposer {
     }
 
     // Methods: RECEIVE(s)
-    public void receivePromise() {
+    public void receivePromise(int _fromUid, ProposalId _proposalId, ProposalId _prevAcceptedId, int _prevAcceptedValue) {
+        if (!proposalId.equals(this.proposalId) || promisesReceived.contains(_fromUid)) {
+            return;
+        }
 
+        promisesReceived.add(_fromUid);
+
+        if (lastAcceptedId == null || _prevAcceptedId.isGreaterThan(lastAcceptedId)) {
+            lastAcceptedId = _prevAcceptedId;
+
+            proposedValue = _prevAcceptedValue;
+        }
+
+        if (promisesReceived.size() == quorumSize) {
+            if (proposedValue != 0) {
+                messenger.sendAccept(this.proposalId, this.proposedValue);
+            }
+        }
     }
 
     public void receiveAccepted() {
 
+    }
+
+    // Other methods
+    public void prepare() {
+        promisesReceived.clear();
+        proposalId.incrementId();
+        messenger.sendPrepare(proposalId);
     }
 }
