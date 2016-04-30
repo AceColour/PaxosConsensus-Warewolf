eab2ccf506c475da0c07ce667b155a512b39eef2
Erick Chandra <erickchandra_1@yahoo.com>
2016-04-30 16:14:31 +0700

    Updated GamePlay.

--- src/GamePlay/Game.java
+++ src/GamePlay/Game.java
@@ -14,6 +14,7 @@ public class Game {
     private HashSet playerWerewolfDeadList = new HashSet();
     private HashSet playerConnected = new HashSet();
     private HashSet playerReady = new HashSet();
+    private HashSet playerLeft = new HashSet();
     boolean startedStatus = false;
 
     // Methods
@@ -24,6 +25,14 @@ public class Game {
 
     public HashSet getPlayerWerewolfActiveList() {
         return playerWerewolfActiveList;
+    }
+
+    public HashSet getPlayerConnected() {
+        return playerConnected;
+    }
+
+    public boolean getStartedStatus() {
+        return startedStatus;
     }
 
     // Other methods
@@ -42,9 +51,20 @@ public class Game {
         while (!found && iterator.hasNext()) {
             currentPlayerIterator = iterator.next();
             if (currentPlayerIterator.getPlayerId() == _playerId) {
-                this.playerCitizenDeadList.add(new Player(currentPlayerIterator.getIpAddress(), currentPlayerIterator.getPortNumber(), currentPlayerIterator.getPlayerId(), currentPlayerIterator.getPlayerRole()));
+                this.playerCitizenDeadList.add(new Player(currentPlayerIterator.getIpAddress(), currentPlayerIterator.getPortNumber(), currentPlayerIterator.getPlayerId(), currentPlayerIterator.getUsername(), false));
                 found = true;
                 iterator.remove();
+
+                // Also set aliveStatus in playerConnected.
+                Iterator<Player> iteratorPlayerConnected = playerConnected.iterator();
+                Player currentPlayerConnectedIterator;
+                boolean foundPlayerConnected = false;
+                while (foundPlayerConnected && iteratorPlayerConnected.hasNext()) {
+                    currentPlayerConnectedIterator = iteratorPlayerConnected.next();
+                    if (currentPlayerConnectedIterator.getPlayerId() == _playerId) {
+                        currentPlayerConnectedIterator.setAliveStatus(false);
+                    }
+                }
             }
         }
 
@@ -52,9 +72,20 @@ public class Game {
         while (!found && iterator.hasNext()) {
             currentPlayerIterator = iterator.next();
             if (currentPlayerIterator.getPlayerId() == _playerId) {
-                this.playerWerewolfDeadList.add(new Player(currentPlayerIterator.getIpAddress(), currentPlayerIterator.getPortNumber(), currentPlayerIterator.getPlayerId(), currentPlayerIterator.getPlayerRole()));
+                this.playerWerewolfDeadList.add(new Player(currentPlayerIterator.getIpAddress(), currentPlayerIterator.getPortNumber(), currentPlayerIterator.getPlayerId(), currentPlayerIterator.getUsername(), false));
                 found = true;
                 iterator.remove();
+
+                // Also set aliveStatus in playerConnected.
+                Iterator<Player> iteratorPlayerConnected = playerConnected.iterator();
+                Player currentPlayerConnectedIterator;
+                boolean foundPlayerConnected = false;
+                while (foundPlayerConnected && iteratorPlayerConnected.hasNext()) {
+                    currentPlayerConnectedIterator = iteratorPlayerConnected.next();
+                    if (currentPlayerConnectedIterator.getPlayerId() == _playerId) {
+                        currentPlayerConnectedIterator.setAliveStatus(false);
+                    }
+                }
             }
         }
     }
@@ -69,8 +100,50 @@ public class Game {
 
     public void clientReady(Player _player) {
         this.playerReady.add(_player);
-        if (this.playerReady.size() == this.playerConnected.size()) {
-            this.startGame();
+    }
+
+    public int clientLeave(String _ipAddr, int _portNo) {
+        // Returns 0: no error
+        // Returns 1: game is playing
+
+        Iterator<Player> iterator = playerConnected.iterator();
+        Player currentPlayerIterator;
+        boolean found = false;
+
+        if (getStartedStatus()) {
+            return 1;
         }
+        else {
+            while (!found && iterator.hasNext()) {
+                currentPlayerIterator = iterator.next();
+                if (currentPlayerIterator.getIpAddress().equals(_ipAddr) && currentPlayerIterator.getPortNumber() == _portNo) {
+                    this.playerLeft.add(new Player(currentPlayerIterator.getIpAddress(), currentPlayerIterator.getPortNumber(), currentPlayerIterator.getPlayerId(), currentPlayerIterator.getUsername(), false));
+
+                    iterator.remove();
+                    found = true;
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    public boolean isPlayerReadyEqualsPlayerConnected() {
+        return (this.playerReady.size() == this.playerConnected.size());
+    }
+
+    public boolean isUsernameExists(String _username) {
+        boolean found = false;
+        Iterator<Player> iterator = playerConnected.iterator();
+        Player currentPlayerIterator;
+
+        while (!found && iterator.hasNext()) {
+            currentPlayerIterator = iterator.next();
+            if (currentPlayerIterator.getUsername().equals(_username)) {
+                found = true;
+            }
+        }
+
+        return found;
     }
 }
